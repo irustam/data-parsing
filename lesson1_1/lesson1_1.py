e5d494795e38590f806c147ec0d021211fa5bf97
irustam <49302555+irustam@users.noreply.github.com>
2019-10-14 10:20:48 +0300

    tasks

--- lesson1_1/lesson1_1.py
+++ lesson1_1/lesson1_1.py
@@ -6,14 +6,22 @@ import requests
 import os
 
 nickname = 'irustam'
-url = f'https://api.github.com/users/{nickname}/repos'
+repos = 1000
+page = 1
+while True:
+    url = f'https://api.github.com/users/{nickname}/repos?per_page={repos}&page={page}'
 
-data = requests.get(url)
+    data = requests.get(url)
+    count = 0
+    if data.status_code == 200:
+        j_data = data.json()
+        for item in j_data:
+            print(item['id'], item['full_name'])
+            count += 1
+        with open(os.path.abspath('lesson1_1.json'), 'a', encoding='utf-8') as f:
+            f.write(data.text)
 
-if data.status_code == 200:
-    j_data = data.json()
-    for item in j_data:
-        print(item['id'], item['full_name'])
-
-    with open(os.path.abspath('lesson1_1.json'), 'w', encoding='utf-8') as f:
-        f.write(data.text)
+    if count < repos:
+        break
+    else:
+        page += 1
